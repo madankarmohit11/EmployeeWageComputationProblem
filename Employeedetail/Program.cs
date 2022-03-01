8b57ea82d59605310218838c1d591c3f63434a28
madankarmohit11 <madankarmohit11@gmail.com>
2022-03-01 20:36:58 +0530

    attendence checking

---
+++ Employeedetail/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FULL_TIME = 1;
+            Random random = new Random();
+            string name = Console.ReadLine();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == FULL_TIME)
+            {
+                Console.WriteLine(name + " is Present");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(name + " is Absent");
+                Console.ReadLine();
+            }
+        }
+    }
+}
